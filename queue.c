595ff6d9a51fc5154c10d687fb043729946a8f40
Satish Kumar Anbalagan <anbalagan.s@northeastern.edu>
2020-09-23 15:01:37 -0400

    added documentation comments and modified indentation

--- queue.c
+++ queue.c
@@ -1,93 +1,113 @@
-/*
-* CS5600 - Computer Systems, Project 0
-*
-* queue.c
-*
-* Author - Satish Kumar Anbalagan
-* NUID - 001351994
-* Email ID - anbalagan.s@northeastern.edu
-*/
+/**
+ * CS5600 - Computer Systems, Project 0
+ *
+ * queue.c
+ * 
+ * Linked queue source file
+ *
+ * Author - Satish Kumar Anbalagan
+ * Date - Sept 17, 2020
+ * Email ID - anbalagan.s@northeastern.edu
+ */
 
 #include "queue.h"
+#define MAX_NUMBER_OF_PROCESS    7    //maximum processes
 
-void initialize(queue_t *queue) {
-  queue->data = NULL;
-  queue->front = NULL;
-  queue->rear = NULL;
+void initialize(queue_t *queue)
+{
+    queue->data = NULL;
+    front = NULL;
+    rear = NULL;
 }
 
-int isempty(queue_t *queue) {
-  return (queue->rear == NULL);
+int isempty(queue_t *queue)
+{
+    return (rear == NULL);
 }
 
-void display(node_t *head) {
-  if(head == NULL) {
-    printf("NULL\n");
-  }
-  else {
-    printf("PID = %d, Name = %s\n", head->p->PID, head->p->name);
-    display(head->next);
-  }
+void display(node_t *head)
+{
+    if(head == NULL) {
+        printf("NULL\n");
+    }
+    else {
+        printf("%s->", head->p->name);
+        display(head->next);
+    }
 }
 
-void enqueue(queue_t *queue, void *element) {
-  node_t *tmp;
-  tmp = malloc(sizeof(node_t));
-  tmp->p = element;
-  tmp->next = NULL;
-  if(!isempty(queue)) {
-      queue->rear->next = tmp;
-      queue->rear = tmp;
-  }
-  else {
-      queue->front = queue->rear = tmp;
-  }
+void enqueue(queue_t *queue, void *element)
+{
+    node_t *tmp;
+    tmp = (node_t *) malloc(sizeof(*tmp));
+    tmp->p = element;
+    tmp->next = NULL;
+    if(!isempty(queue)) {
+        rear->next = tmp;
+        rear = tmp;
+    }
+    else {
+        queue->data = front = rear = tmp;
+    }
 }
 
-void* dequeue(queue_t *queue) {
-  node_t *tmp;
-  process_t* n = queue->front->p;
-  tmp = queue->front;
-  queue->front = queue->front->next;
-  free(tmp);
-  return n;
+void* dequeue(queue_t *queue)
+{
+    node_t *tmp;
+    process_t* n = front->p;
+    tmp = front;
+    front = front->next;
+    queue->data = front;
+    free(tmp);
+    return n;
 }
 
-int main() {
-  queue_t *queue;
-  queue = malloc(sizeof(queue_t)); 
-  initialize(queue);
+int main()
+{
+    queue_t *queue;
+    queue = (queue_t *) malloc(sizeof(*queue)); 
+    initialize(queue);
 
-  printf("\n");
-  printf("##########################\n");
-  printf("#  Queue before Enqueue  #\n");
-  printf("##########################\n");
-  display(queue->front);
+    printf("\n");
+    printf("##########################\n");
+    printf("#  Queue before Enqueue  #\n");
+    printf("##########################\n");
+    display(queue->data);
 
-  process_t process[3];
-  for(int i=0;i<3;i++) {
-    process_t *p = &process[i];
-    p->PID = i;
-    p->name = malloc(sizeof(*p->name)*9);
-    sprintf(p->name, "Process%d", i);
-    enqueue(queue, p);
-    printf("\nQueue after enqueue %d - \n", i);
-    display(queue->front);
-  }
+    printf("\n");
+    printf("##########################\n");
+    printf("#         Enqueue        #\n");
+    printf("##########################\n");
 
-  printf("\n");
-  printf("##########################\n");
-  printf("#  Queue before Dequeue  #\n");
-  printf("##########################\n");  
-  display(queue->front);
+    process_t process[MAX_NUMBER_OF_PROCESS];
+    for(int i=0;i<MAX_NUMBER_OF_PROCESS;i++) {
+        process_t *p = &process[i];
+        p->PID = i;
+        p->name = malloc(sizeof(*p->name)*9);
+        sprintf(p->name, "Process%d", i);
+        enqueue(queue, p);
+        printf("Enqueued %s .....\t", p->name);
+        display(queue->data);
+    }
 
-  for(int i=0;i<3;i++) {
-    dequeue(queue);
-    printf("\nQueue after dequeue %d - \n", i);
-    display(queue->front);
-  }
-  
-  free(queue);
-  return 0;
+    printf("\n");
+    printf("##########################\n");
+    printf("#  Queue before Dequeue  #\n");
+    printf("##########################\n");  
+    display(queue->data);
+
+    printf("\n");
+    printf("##########################\n");
+    printf("#         Dequeue        #\n");
+    printf("##########################\n"); 
+
+    for(int i=0;i<MAX_NUMBER_OF_PROCESS;i++) {
+        process_t* dequeuedProcess =  dequeue(queue);
+        printf("Dequeued %s .....\t", dequeuedProcess->name);
+        display(queue->data);
+    }
+
+    free(queue);
+    return 0;
 
 }
--- queue.h
+++ queue.h
@@ -1,41 +1,77 @@
-/*
-* CS5600 - Computer Systems, Project 0
-*
-* queue.h
-*
-* Author - Satish Kumar Anbalagan
-* NUID - 001351994
-* Email ID - anbalagan.s@northeastern.edu
-*/
+/**
+ * CS5600 - Computer Systems, Project 0
+ *
+ * queue.h
+ * 
+ * Linked queue header file
+ *
+ * Author - Satish Kumar Anbalagan
+ * Date - Sept 17, 2020
+ * Email ID - anbalagan.s@northeastern.edu
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 //Type definitions
 
+/**
+ * Type definition of a Process with its identifier and name field.
+ */
 typedef struct _process {
-  int PID;
-  char *name;
+    int PID;    //process identifier
+    char *name;    //process name
 } process_t;
 
+/**
+ * Type definition of linked queue node with process  and next node field.
+ */
 typedef struct _node {
-  process_t* p;
-  struct _node *next;
+    process_t* p;    //current process
+    struct _node *next;
 } node_t;
 
+/**
+ * Type definition of generic queue
+ */
 typedef struct _queue {
-  void *data;
-  node_t *front;
-  node_t *rear;
+    void *data;
 } queue_t;
 
+node_t *front;    // node front in queue
+node_t *rear;    // node behind in queue
+
 //Function declarations
+
+/**
+ * Checks if the queue is empty.
+ * @param queue - pointer to the linked queue
+ * @return int if queue is empty return's 1, else 0 
+ */
 int isempty(queue_t *queue);
 
+/**
+ * initializes the linked queue to NULL.
+ * @param queue - pointer to the linked queue 
+ */
 void initialize(queue_t *queue);
 
+/**
+ * displays the contents of the node in the linked queue.
+ * @param head - pointer to the node in the linked queue
+ */
 void display(node_t *head);
 
+/**
+ * function to add elements to the linked queue.
+ * @param queue - pointer to the linked queue
+ * @param element - pointer to the process in the node
+ */
 void enqueue(queue_t *queue, void *element);
 
+/**
+ * function to remove elements from the linked queue.
+ * @param queue - pointer to the linked queue
+ * @return void pointer - here, process_t removed
+ */
 void* dequeue(queue_t *queue);
